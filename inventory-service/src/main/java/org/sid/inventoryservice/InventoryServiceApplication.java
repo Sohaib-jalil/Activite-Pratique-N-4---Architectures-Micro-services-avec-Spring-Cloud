69c6fe4d231a4b0015c02bf11af48c3778912ed2
Sohaib-jalil <jalil.sohaib01@gmail.com>
2023-11-22 21:50:55 +0100

    Video 2

--- inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/sid/inventoryservice/InventoryServiceApplication.java
@@ -1,13 +1,34 @@
 package org.sid.inventoryservice;
 
+import org.sid.inventoryservice.entities.Product;
+import org.sid.inventoryservice.repo.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner start(ProductRepository productRepository) {
+        return args -> {
+            for (int i = 1; i < 10; i++) {
+                productRepository.save(Product.builder()
+                        .name("Ordinateur" + i)
+                        .price(Math.random() * 10000)
+                        .quantity((int) (Math.random() * 100))
+                        .build());
+            }
+            productRepository.findAll().forEach(System.out::println);
+        };
+    }
 }
--- order-service/src/main/java/org/sid/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/org/sid/orderservice/OrderServiceApplication.java
@@ -1,13 +1,66 @@
 package org.sid.orderservice;
 
+import org.sid.orderservice.entities.Order;
+import org.sid.orderservice.entities.ProductItem;
+import org.sid.orderservice.enums.OrderStatus;
+import org.sid.orderservice.model.Customer;
+import org.sid.orderservice.model.Product;
+import org.sid.orderservice.repo.OrderRepository;
+import org.sid.orderservice.repo.ProductItemRepository;
+import org.sid.orderservice.services.CustomerRestClientService;
+import org.sid.orderservice.services.InventoryRestClientService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OrderServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderServiceApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner start(ProductItemRepository productItemRepository,
+                            OrderRepository orderRepository,
+                            CustomerRestClientService customerRestClientService,
+                            InventoryRestClientService inventoryRestClientService) {
+        return args -> {
+            List<Customer> customers = customerRestClientService.allCustomers().getContent().stream().toList();
+            List<Product> product = inventoryRestClientService.allProducts().getContent().stream().toList();
+            Long customerId = 1L;
+            Random random = new Random();
+            Customer customer = customerRestClientService.customerById(customerId);
+            for (int i = 0; i < 20; i++) {
+                Order order = Order.builder().
+                        customerId(customers.get(random.nextInt(customers.size())).getId())
+                        .status(Math.random() > 0.5 ? OrderStatus.PENDING : OrderStatus.CREATED)
+                        .createdAt(new Date())
+                        .build();
+                Order saveOrder = orderRepository.save(order);
+                for (int j = 0; j < product.size(); j++) {
+                    if (Math.random() > 0.7) {
+                        ProductItem productItem = ProductItem.builder()
+                                .order(saveOrder)
+                                .productId(product.get(j).getId())
+                                .price(product.get(j).getPrice())
+                                .quantity(1+random.nextInt(10))
+                                .discount(Math.random())
+                                .build();
+                        productItemRepository.save(productItem);
+                    }
+                }
+            }
+        };
+
     }
 
 }
